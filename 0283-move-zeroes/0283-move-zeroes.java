9b06f2af67e55ea85b8fbed10bce666864124089
anuj15102002 <142865884+anuj15102002@users.noreply.github.com>
2025-02-22 21:49:57 +0530

    Time: 2 ms (84.57%), Space: 46.4 MB (5.66%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,11 +1,19 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+
+        //we'll have a pointer at the start of the array 
        int i=0;
+
+        //now iterate theough the nums
        for(int j=0;j<nums.length;j++){
+
+        //check if the nums[j] is not the zero
         if(nums[j]!=0){
+            //then swap the element with i index value
             int temp=nums[i];
             nums[i]=nums[j];
             nums[j]=temp;
+            //and increase i by 1
             i++;
         }
        }
