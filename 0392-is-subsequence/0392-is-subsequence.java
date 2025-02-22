a0f58c878bb13bf46d5697183f2a9774eac15992
anuj15102002 <142865884+anuj15102002@users.noreply.github.com>
2025-02-22 22:06:53 +0530

    Time: 2 ms (65.44%), Space: 41.3 MB (85.26%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,15 +1,25 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        int i=0;
-        int j=0;
-        while(i<s.length() && j<t.length())
-        {
-            if(s.charAt(i)==t.charAt(j)){
-                i++;
-            }
-            j++;
+        int l=0, r=0;
+
+        //we iterate till the l is less than s(length) and r < t(length)
+        while(l<s.length() && r<t.length()){
+
+            //if(char at l from s stringis euqal to char at r from t string)
+            //then we increase l by 1
+            if(s.charAt(l)==t.charAt(r))l++;
+
+            //otherwise we incrase the r by 1
+            r++;
         }
-        return (i==s.length());
+
+        //if l reach to the length of s string then 
+        //then it means all the char from s string are present in t string
+        if(l==s.length())return true;
+
+        //if they are not present then l will iterate to the length 
+        //of s string
+        return false;
         
     }
 }
