e9fb59b6a324ab42bf9da30db05bb7986f481b3f
anuj15102002 <142865884+anuj15102002@users.noreply.github.com>
2025-02-22 22:34:37 +0530

    Time: 5 ms (74.2%), Space: 58.2 MB (22.57%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,17 +1,27 @@
 class Solution {
     public int maxArea(int[] height) {
-        int maxArea=0;
-        int i=0;
-        int j=height.length-1;
-        while(i<j)
-        {
-            int newArea=Math.min(height[i],height[j])*(j-i);
-            maxArea=Math.max(maxArea,newArea);
-            if(height[i]>height[j])j--;
-            else i++;
+        //we have two pointer s and e
+        //and a variable for maxArea
+        int s=0, max=0;
+        int e=height.length-1;
 
-        }
-        return maxArea;
-        
+        while(s<e){
+            //variable to store current area
+            //find min bcoz it will store atmost at that height (l) 
+            //and breadth (e-s)
+            int area=Math.min(height[s],height[e])*(e-s);
+
+            //compare it with max
+            max=Math.max(area,max);
+
+            //if the height of e index is greater then we can move
+            //e pointer to left to find the bigger line
+            if(height[e]>height[s])s++;
+
+            //if not then move e to right to find the bigger line
+            else e--;
+
+            }
+            return max;
     }
 }
