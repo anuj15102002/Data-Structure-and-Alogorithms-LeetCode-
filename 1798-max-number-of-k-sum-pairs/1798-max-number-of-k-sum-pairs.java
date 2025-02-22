1697ebcfc84254edd44ff9212a0cda71e0da2f90
anuj15102002 <142865884+anuj15102002@users.noreply.github.com>
2025-02-22 23:42:59 +0530

    Time: 39 ms (18.47%), Space: 56.2 MB (78.05%) - LeetHub

--- 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.java
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.java
@@ -1,20 +1,36 @@
 class Solution {
     public int maxOperations(int[] nums, int k) {
-        Map<Integer,Integer> ans=new HashMap();
-        int count=0;
-        for(int n:nums)
+        Map<Integer,Integer> result=new HashMap<>();
+        // hashmap to store value
+        //and number of times they have not been used to find 
+
+        int count=0; //to count the number of pairs have sum equal to k
+        
+        for(int i=0;i<nums.length;i++)
         {
-            int target=k-n;
-            if(ans.containsKey(k-n) && ans.get(target)>0){
+            //take difference of k and current index value to store in target
+            int target=k-nums[i];
+
+            //check if target is there in map and 
+            //if the no. of count it has not been used is greater than 0
+            //then increase count by 1 and decrease the number of that
+            //value not used by 1
+            if(result.containsKey(target) && result.get(target)>0){
                 count++;
-                ans.put(target,ans.getOrDefault(target,0)-1);
+                result.put(target,result.getOrDefault(target,0)-1);
             }
+
+            //otherwise we put the current index value to map
+            //and increase the number of not used by 1 or
+            //if it already exits then incrase not used number by 1
             else
             {
-              ans.put(n,ans.getOrDefault(n,0)+1);
-
+                result.put(nums[i],result.getOrDefault(nums[i],0)+1);
             }
         }
+
+        //and in the end we returns the count
         return count;
+        
     }
 }
