cd5bf795819d89d80e634437f7b98148dde02340
anuj15102002 <142865884+anuj15102002@users.noreply.github.com>
2025-02-23 13:13:07 +0530

    Time: 1 ms (83.77%), Space: 41.7 MB (84.81%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.java
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -1,17 +1,22 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-     StringBuilder result=new StringBuilder();
-     int i=0;
-     while(i<word1.length() || i<word2.length()){
-        if(i<word1.length()){
-            result.append(word1.charAt(i));
+
+        //we use stringBuilder to store the characters alternately
+        StringBuilder sb=new StringBuilder();
+        int i=0;
+        while(i<word1.length() | i<word2.length())
+        {
+            //if the word1 length is not passed then we append word1 char
+            //first and then char of word2 in sb until the length of both 
+            //string is surpased by i
+
+            if(i<word1.length())sb.append(word1.charAt(i));
+            if(i<word2.length())sb.append(word2.charAt(i));
+            i++; 
         }
-        if(i<word2.length()){
-            result.append(word2.charAt(i));
-        }
-        i++;
-     }
-     return result.toString();
+
+        //now we convert the stringBuilder to string and returns it
+        return sb.toString();
         
     }
 }
