47e3b0c5d4c8731d95cbdec8f24cb62b8d6a7c35
anuj15102002 <142865884+anuj15102002@users.noreply.github.com>
2025-02-23 13:44:42 +0530

    Time: 3 ms (82.61%), Space: 45.1 MB (60.09%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,26 +1,44 @@
 class Solution {
     public String reverseVowels(String s) {
-        int n=s.length()-1;
-        int st=0;
-        char[] arr=s.toCharArray();
-        while(st<n)
+
+        //here we have to reverse the vowel positions at the same order
+        //without changing the positions of now vowel characters
+
+        //so here we use two pointer
+        //start and at one at end
+        //we move start pointer to the first vowel until it doesn't crosses
+        //end pointer
+        //move end pointer to first vowel from the right to left until it 
+        //crosses start pointer
+        //then swap both the vowels and increase start by 1 and decrease 
+        //end by 1
+
+        //but first string into charArray to have easy access of characters
+
+        char[] text=s.toCharArray();
+        int start=0;
+        int end=text.length-1;
+        String vowels="aeiouAEIOU";
+
+        while(start<end)
         {
-            if(!isVowel(arr[st]))st++;
-            else if(!isVowel(arr[n]))n--;
-            else {
-                char temp=arr[st];
-                arr[st]=arr[n];
-                arr[n]=temp;
-                st++;
-                n--;
-            }
+            //move the s pointer until it points to vowel
+            while(start<end && vowels.indexOf(text[start]) == -1)start++;
+
+            //move e pointer until it points to vowel
+            while(start<end && vowels.indexOf(text[end])==-1)end--;
+            
+            //swap the vowels
+            char temp=text[start];
+            text[start]=text[end];
+            text[end]=temp;
+
+            start++;
+            end--;
         }
-        return String.valueOf(arr);
+        // return the string by creating with new keyword
+        return new String(text);
+
+        
     }
-     public static boolean isVowel(char ch)
-     {
-        if(ch=='a' || ch=='e' || ch=='i'|| ch=='o'|| ch=='u'|| ch=='A'|| ch=='E'|| ch=='I'|| ch=='O'|| ch=='U')return true;
-        return false;
-     }   
-    
 }
