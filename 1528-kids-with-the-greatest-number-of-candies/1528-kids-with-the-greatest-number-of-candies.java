0e68dc36897b47f074f6ae7149783087645cc2ae
anuj15102002 <142865884+anuj15102002@users.noreply.github.com>
2025-02-23 13:23:54 +0530

    Time: 1 ms (87.7%), Space: 42.5 MB (55.68%) - LeetHub

--- 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.java
+++ 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.java
@@ -1,12 +1,20 @@
 class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+
+        //first we find the maxCandies among the candies array 
+        //then comapre with each kid candies + extraCanides is Greater than
+        //the maxCandies or not
+        //and store the boolean in arrayList
+
+        List<Boolean> result=new ArrayList<>();
         int maxCandies=0;
         for(int candy:candies)
         {
-            maxCandies=Math.max(maxCandies,candy);
+            maxCandies=Math.max(candy,maxCandies);
         }
-        List<Boolean> result=new ArrayList<>();
-        for(int candy:candies){
+
+        for(int candy:candies)
+        {
             if(candy+extraCandies>=maxCandies)result.add(true);
             else result.add(false);
         }
