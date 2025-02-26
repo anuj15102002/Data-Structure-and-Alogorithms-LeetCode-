d94bf9ae48b43a2e9283c67ff02529c2f9792df1
anuj15102002 <142865884+anuj15102002@users.noreply.github.com>
2025-02-26 19:24:39 +0530

    Time: 1 ms (99.08%), Space: 44.6 MB (85.91%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,26 +1,54 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
 
-        if(magazine.length()<ransomNote.length())return false;
+        //FIRST METHOD
+        
+        //first we creaet an integer array to store the occurance of all the
+        //the character in magazines (a-z)
+        int[] occ=new int[26];
 
-        char[] r=ransomNote.toCharArray();
-        char[] m=magazine.toCharArray();
-        Map<Character,Integer> c=new HashMap<>();
-        int count=0;
-        for(char n:m)
+        //now first we count occurace of each char in magazine 
+        for(char ch:magazine.toCharArray())
         {
-            c.put(n,c.getOrDefault(n,0)+1);
+            occ[ch - 'a']++; // means char(asccci code) - 97
+            //if its a then its occurance will be stored in 0th index
         }
-        for(char n:r)
+        //then we traverse ransomNote array and decrease the occurances of
+        //each char
+
+        for(char ch:ransomNote.toCharArray())
         {
-            if(c.containsKey(n) && c.get(n)>0)
-            {
-                count++;
-                c.put(n,c.get(n)-1);
-            }
+            occ[ch - 'a']--;
+            if(occ[ch - 'a']<0)return false; //means there are no more char   in magazine
+                                //to make the fulfill (aaa , aab)
+                                //then it will go in negative
         }
-        if(count==r.length)return true;
-        return false;
+
+        return true;
+
+
+        //  SECOND METHOD WITH HASHMAP
+
+        // if(magazine.length()<ransomNote.length())return false;
+
+        // char[] r=ransomNote.toCharArray();
+        // char[] m=magazine.toCharArray();
+        // Map<Character,Integer> c=new HashMap<>();
+        // int count=0;
+        // for(char n:m)
+        // {
+        //     c.put(n,c.getOrDefault(n,0)+1);
+        // }
+        // for(char n:r)
+        // {
+        //     if(c.containsKey(n) && c.get(n)>0)
+        //     {
+        //         count++;
+        //         c.put(n,c.get(n)-1);
+        //     }
+        // }
+        // if(count==r.length)return true;
+        // return false;
 
     }
 }
