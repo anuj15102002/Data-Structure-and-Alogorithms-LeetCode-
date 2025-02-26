96c259c9c8953dc2c8c86d9fb147380033f79390
anuj15102002 <142865884+anuj15102002@users.noreply.github.com>
2025-02-26 19:06:54 +0530

    Time: 11 ms (46.08%), Space: 45.3 MB (40.83%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        char[] r=ransomNote.toCharArray();
+        char[] m=magazine.toCharArray();
+        Map<Character,Integer> c=new HashMap<>();
+        int count=0;
+        for(char n:m)
+        {
+            c.put(n,c.getOrDefault(n,0)+1);
+        }
+        for(char n:r)
+        {
+            if(c.containsKey(n) && c.get(n)>0)
+            {
+                count++;
+                c.put(n,c.get(n)-1);
+            }
+        }
+        if(count==r.length)return true;
+        return false;
+
+    }
+}
