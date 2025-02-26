cb4a328d3037616a4b61e81e9ed9c0e083aa53e3
anuj15102002 <142865884+anuj15102002@users.noreply.github.com>
2025-02-26 20:01:11 +0530

    Time: 10 ms (75.26%), Space: 42.6 MB (37.23%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,7 +1,9 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
         if(s.length()!=t.length())return false;
+        
 
+        //first we create map to store the original and replacement char
         Map<Character,Character> map=new HashMap<>();
         char[] original=s.toCharArray();
         char[] replacement=t.toCharArray();
@@ -9,20 +11,29 @@ class Solution {
 
         for(int i=0;i<s.length();i++)
         {
-        
+            //if map does not contains orignial char as a key then
             if(!map.containsKey(original[i]))
             {
+                //if map contains replacement char as a value
                 if(!map.containsValue(replacement[i])){
+                    //then we put original and replacement char to map
                     map.put(original[i],replacement[i]);
                 }
+                //if the map contains replacement char as value then
+                //we are making replacement of same char with two diff. char
                 else return false;
             }
+            //if map contains original char as a key
             else
             {
+                //first we check if the replacement char is same or not 
+                //as we in the map that is stored previously
                 char v=map.get(original[i]);
                 if(replacement[i]!=v)return false;
             }
         }
+
+        //if everything goes without any trouble then
         return true;
         
     }
