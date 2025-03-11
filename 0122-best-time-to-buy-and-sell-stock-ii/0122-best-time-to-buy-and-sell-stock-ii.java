7c9c6ec5a85e25ad841b12d076a9b64fbc36494f
anuj15102002 <142865884+anuj15102002@users.noreply.github.com>
2025-03-11 12:20:58 +0530

    Time: 1 ms (76.71%), Space: 45.5 MB (92.87%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,5 +1,10 @@
 class Solution {
     public int maxProfit(int[] prices) {
+        //int this problem we have to just check if current number
+        //should be greater than its provious number
+        //then we take the difference of these two and add it to the 
+        //profit and returns it
+        
         int profit=0;
         for(int i=1;i<prices.length;i++)
         {
