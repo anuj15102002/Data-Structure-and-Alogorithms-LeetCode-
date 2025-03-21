66c3d5976d3d2e3cff893e9c13548ae6912c71b9
anuj15102002 <142865884+anuj15102002@users.noreply.github.com>
2025-03-21 18:55:21 +0530

    Time: 0 ms (100%), Space: 43.3 MB (43.6%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
@@ -10,52 +10,30 @@
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        if (head == null || head.next == null)
-            return head;
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        ListNode prev = dummy;
-        ListNode curr = head;
-        while (curr != null && curr.next != null) {
-            ListNode nextNode = curr.next;
-            if (curr.val == nextNode.val) {
-                while (nextNode != null && curr.val == nextNode.val) {
-                    curr = curr.next;
-                    nextNode = curr.next;
-                }
-                prev.next = nextNode;
-            } else {
-                prev = prev.next;
-            }
-            curr = curr.next;
 
-            
+        ListNode dummy=new ListNode(0);
+        dummy.next=head;
+        ListNode pre=dummy;
+        ListNode curr=head;
+        
+        while(curr!=null )
+        {
+            if(curr.next!=null && curr.val==curr.next.val)
+            {
+                while(curr.next!=null && curr.val== curr.next.val)
+                {
+                    curr=curr.next;
+                }
+                pre.next=curr.next;
+            }
+            else
+            {
+                pre=pre.next;
+            }
+            curr=curr.next;
         }
+      
         return dummy.next;
-
-    }
-
-    public ListNode deleteDuplicatesWithoutDummy(ListNode head) {
-        if (head == null || head.next == null)
-            return head;
-        ListNode currNode = head, prevNode = null;
-        while (currNode != null && currNode.next != null) {
-            ListNode nextNode = currNode.next;
-            if (currNode.val != nextNode.val) {
-                prevNode = currNode;
-                currNode = currNode.next;
-            } else {
-                while (nextNode != null && currNode.val == nextNode.val) {
-                    nextNode = nextNode.next;
-                }
-                if (prevNode == null)
-                    head = nextNode;
-                else
-                    prevNode.next = nextNode;
-                currNode = nextNode;
-            }
-        }
-        return head;
         
     }
 }
