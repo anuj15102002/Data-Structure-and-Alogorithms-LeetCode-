b033415007a91cb1d8cec284a61a93e55e8b216f
Anuj Gautam <142865884+anuj15102002@users.noreply.github.com>
2026-02-02 03:42:33 +0530

    Time: 15 ms (56.35%), Space: 48.6 MB (53.62%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,26 +1,16 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-        int n = nums.length;
-        int[] nge = new int[n];
-        Stack<Integer> st = new Stack<>(); // will store values (could store indices too)
-
-        // Iterate from right to left across a logical array of length 2*n
-        for (int i = 2 * n - 1; i >= 0; i--) {
-            int current = nums[i % n];
-
-            // Pop all elements that are <= current, because they can't be next greater
-            while (!st.isEmpty() && st.peek() <= current) {
-                st.pop();
-            }
-
-            // Only fill results when i < n (first pass for original positions)
+        int n=nums.length;
+        int[] ans = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        for(int i=2*n-1;i>=0;i--){
+            int current = nums[i%n];
+            while(!stack.isEmpty() && stack.peek()<=nums[i%n])stack.pop();
             if (i < n) {
-                nge[i] = st.isEmpty() ? -1 : st.peek();
+               ans[i] = stack.isEmpty() ? -1 : stack.peek();
             }
-
-            // Push current as a candidate for earlier elements
-            st.push(current);
+            stack.push(current);
         }
-        return nge;
+        return ans;
     }
 }
